f896e23afb002dc16eee8ec68ed48adc05ba3382
Seth Parson <defectiveseth@gmail.com>
2014-04-24 14:17:43 -0400

    update

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?
-
-echo "Hi";
-
+$page_url="http";
+if(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') $page_url .= 's';
+echo $page_url.'://'.$_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI'];
+echo "  ($page_url) ";
 ?>

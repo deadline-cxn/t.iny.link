41dded460c9c41e4fc99e1d3fe00da9c43d666b6
Seth Parson <defectiveseth@gmail.com>
2014-04-24 14:30:08 -0400

    update

--- index.php
+++ index.php
@@ -4,11 +4,7 @@ if(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') $page_url .= 's';
 $page_url.='://'.$_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI'];
 $xpage=explode("/",$page_url);
 $link=$xpage[count($xpage)-1];
-
-echo "[$link]";
-
-if( ($link=="iny.link") || 
-	($link=="t.iny.link")) {
+if(empty($link)) {
 	echo "Make a tiny link:<br>";
 	echo "<form method=post>LONG URL: <input name=url><input type=submit></form>";
 	echo "Copyright (C)2014 Seth Parson<br>";

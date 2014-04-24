3503d6209171081fd884f87268ba0f8420e43828
Seth Parson <defectiveseth@gmail.com>
2014-04-24 14:31:36 -0400

    update

--- index.php
+++ index.php
@@ -1,15 +1,21 @@
 <?
-$page_url="http";
-if(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') $page_url .= 's';
-$page_url.='://'.$_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI'];
-$xpage=explode("/",$page_url);
-$link=$xpage[count($xpage)-1];
-if(empty($link)) {
-	echo "Make a tiny link:<br>";
-	echo "<form method=post>LONG URL: <input name=url><input type=submit></form>";
-	echo "Copyright (C)2014 Seth Parson<br>";
+$url=$_REQUEST['url'];
+if(!empty($url)) {
+	echo "[$url]";
 }
 else {
-	echo $link;
+	$page_url="http";
+	if(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') $page_url .= 's';
+	$page_url.='://'.$_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI'];
+	$xpage=explode("/",$page_url);
+	$link=$xpage[count($xpage)-1];
+	if(empty($link)) {
+		echo "Make a tiny link:<br>";
+		echo "<form method=post>LONG URL: <input name=url><input type=submit></form>";
+		echo "Copyright (C)2014 Seth Parson<br>";
+	}
+	else {
+		echo $link;
+	}
 }
 ?>

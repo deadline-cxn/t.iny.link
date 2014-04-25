a0dbf2e0b88b9aa287927261d61e89967d8ce3b7
Seth Parson <defectiveseth@gmail.com>
2014-04-25 09:45:26 -0400

    update

--- index.php
+++ index.php
@@ -1,7 +1,18 @@
 <?
+$link = mysqli_connect("localhost", "sethcode_iny", "inylink", "sethcode_iny");
+if(mysqli_connect_errno()) {
+	printf("Connect failed: %s\n", mysqli_connect_error());
+	exit();
+}
+// if(mysqli_query($link, "CREATE TEMPORARY TABLE myCity LIKE City") === TRUE) { 	printf("Table myCity successfully created.\n"); }
+if($result = mysqli_query($link, "SELECT Name FROM City LIMIT 10")) { 	printf("Select returned %d rows.\n", mysqli_num_rows($result)); 	mysqli_free_result($result); }
+if($result = mysqli_query($link, "SELECT * FROM City", MYSQLI_USE_RESULT)) {     if(!mysqli_query($link, "SET @a:='this will not work'")) {         printf("Error: %s\n", mysqli_error($link));    }     mysqli_free_result($result);}
+// mysqli_close($link);
+
 $url=$_REQUEST['url'];
 if(!empty($url)) {
 	echo "[$url]";
+	
 }
 else {
 	$page_url="http";
@@ -18,4 +29,7 @@ else {
 		echo $link;
 	}
 }
+
+
+
 ?>

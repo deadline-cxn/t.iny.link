0cd9961933cc7991509b0172d403edaff0f96509
Seth Parson <defectiveseth@gmail.com>
2014-04-25 11:17:03 -0400

    update

--- index.php
+++ index.php
@@ -1,39 +1,60 @@
 <?
-$db = new mysqli("localhost", "sethcode_iny", "inylink", "sethcode_iny");
-if($db->connect_errno > 0){ die('Unable to connect to database [' . $db->connect_error . ']'); }
-$url=$_REQUEST['url'];
 
+if (isset($argv[1])) {
+    inc_c($argv[1],strlen($argv[1])-1);
+	exit();
+}
 
 function inc_c($code,$codeloc) {
 	$codepool="0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-_.";
-	echo "code[$code]<br>";
-	echo "codepool[$codepool]<br>";
-	echo "what[".$code[$codeloc]."]<br>";
+	//echo "code[$code]\n";
+	//echo "codepool[$codepool]\n";
+	//echo "what[".$code[$codeloc]."]\n";	
 	$x=strpos($codepool,$code[$codeloc]);
+	$ox=$x;
 	$x++;
-	echo "Old[".$codepool[$x-1]."] <br>";
-	echo "New[".$codepool[$x]."] <br>";
+	if($x>strlen($codepool)-1) {
+		$x=0;
+		if($codeloc==0) {
+			$code=$code.$codepool[$x];
+		}
+		else {			
+			$code=inc_c($code,$codeloc-1);
+		}
+	}
+	//echo "Old[".$codepool[$ox]."]\n";
+	//echo "New[".$codepool[$x]."]\n";
+	$code[$codeloc]=$codepool[$x];
+	//echo $code."\n";
+	return $code;
 }
+$db = new mysqli("localhost", "sethcode_iny", "inylink", "sethcode_iny");
+if($db->connect_errno > 0){ die('Unable to connect to database [' . $db->connect_error . ']'); }
 
-if(!empty($url)) {
-	echo "url=[$url]<br>";
+$url=$_REQUEST['url'];
+if(!empty($url)) {	
+	// echo "url=[$url]<br>";	
+	$result = $db->query("select * from `link` where `url`='$url'");
+	$lnk=$result->fetch_object();
+	if(!empty($lnk->url)) {
+		echo "Exists!";
+	}
+	else {
+		echo "Does not exist";
+	}
+	
+	
+	
+	
 	$result = $db->query("select * from `system` where `var`='code'");
 	while($row = $result->fetch_assoc()){
 		echo $row['var'] . " = [" . $row['val'] ."]<br>";
 		$code=$row['val'];
 	}
-	$x=strlen($code);
-	echo "code_length:[$x]<br>";
-	$x--;
-	// echo "codepool[$codepool]<br>";
-	// $codeloc=strpos($codepool,$code);
-	// echo "codeloc[$codeloc]<br>";
-	// $newcodeloc=$codeloc+1;
-	// echo "newcodeloc[$newcodeloc]<br>";
-	
-	$code=inc_c($code,$x);
-	
-	
+	echo "old_code[$code]<br>";
+	$code=inc_c($code,strlen($code)-1);
+	echo "new_code[$code]<br>";
+		
 	
 /*	code
 	url

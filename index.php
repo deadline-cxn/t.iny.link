8a85e48a76fcaabe1aa76ab61630b2117c3ffa85
Seth Parson <defectiveseth@gmail.com>
2014-05-15 20:51:38 -0500

    update

--- index.php
+++ index.php
@@ -1,5 +1,17 @@
 <?
 include(".config.php"); // .config.php stores $dbhost, $dbdb, $dbpass, and $dbname for mysql operations
+?>
+<html><head><title>t.iny.link - shorten your urls</title></head><body> 
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+  ga('create', 'UA-51059708-1', 'iny.link');
+  ga('send', 'pageview');
+</script>
+<?
+
 if(isset($argv[1])) { inc_c($argv[1],strlen($argv[1])-1); exit(); }
 function put_ads() {  echo "<script async src=\"//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js\"></script><!-- hey --><ins class=\"adsbygoogle\" style=\"display:inline-block;width:728px;height:90px\" data-ad-client=\"ca-pub-9784595369821502\" data-ad-slot=\"9276856171\"></ins> <script> (adsbygoogle = window.adsbygoogle || []).push({}); </script><script type=\"text/javascript\">	var _gaq = _gaq || []; 	_gaq.push(['_setAccount', 'UA-36907330-2']); _gaq.push(['_trackPageview']); (function() { var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true; ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js'; var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s); })(); </script>"; }
 function inc_c($code,$codeloc) {
@@ -28,30 +40,36 @@ if(!empty($act)) {
 				// if(empty($l->submit_ip)) $l->submit_ip="(unknown)";
 				// if($l->submit_date=="0000-00-00 00:00:00") $l->submit_date="(unknown)";
 			echo"<tr><td>http://t.iny.link/$l->code</td><td>$l->hits</td><td>$l->url </td></tr>";
-		} 
+		}
 		echo "</table>";
 		exit; 
 	}
 }
+
 $url=$_REQUEST['url'];
+
 if(!empty($url)) {
 	$result = $db->query("select * from `link` where `url`='$url'");
 	$lnk=$result->fetch_object();
 	if(!empty($lnk->url)) { }
 	else {
 		$result = $db->query("select * from `system` where `var`='code'");
-		while($row = $result->fetch_assoc()){ $code=$row['val']; }
+		while($row = $result->fetch_assoc()) {
+			$code=$row['val'];
+		}
 		$code=inc_c($code,strlen($code)-1);
 		if(empty($lnk->code)) {
 			$sip=$_SERVER['REMOTE_ADDR'];
 			$result=$db->query("insert into `link` (`url`,`code`,`submit_ip`) values ('$url','$code','$sip');");
 			$result=$db->query("update `system` set `val`='$code' where `var`='code'");
-		} else { $code=$lnk->code; }
+		}
+		else {
+			$code=$lnk->code;
+		}
 		$result=$db->query("select * from `link` where `code`='$code'");
 		$lnk=$result->fetch_object();
 	}
-	echo "<a href=http://t.iny.link/$lnk->code>http://t.iny.link/$lnk->code</a><br>";
-	put_ads();
+	echo "<br>$lnk->url =  <a href=http://t.iny.link/$lnk->code>http://t.iny.link/$lnk->code</a><br>";// 	put_ads();
 }
 else {
 	$page_url="http";
@@ -60,10 +78,6 @@ else {
 	$xpage=explode("/",$page_url);
 	$code=$xpage[count($xpage)-1];
 	if(empty($code)) {
-		echo "<html><head>";
-		echo "<title>t.iny.link - shorten your urls</title>";
-		echo "<link rel=\"stylesheet\" href=\"t.css\" type=\"text/css\">";
-		echo "</head><body>";
 		echo "<style>";
 		echo "body { background-color: #aaf; } h1 { font-size: xx-large; color: black; }";
 		echo "</style>";
@@ -71,8 +85,7 @@ else {
 		echo "<h1>Make a t.iny.link!</h1>";
 		echo "<form method=post>LONG URL:<input name=url size=80><input type=submit></form>";
 
-		echo "<a href=\"https://chrome.google.com/webstore/detail/tinylink/faejefmbcehfegajdeliofenfneodopa/details\" target=_blank><img src=favicon.png border=0 width=32 height=32>Install Google Chrome 
-Extension</a><br><br><br>";
+		echo "<a href=\"https://chrome.google.com/webstore/detail/tinylink/faejefmbcehfegajdeliofenfneodopa/details\" target=_blank><img src=favicon.png border=0 width=32 height=32>Install Google Chrome Extension</a><br><br><br>";
 
 		put_ads();
 		echo "<p>&nbsp;</p>";
